3a1b6c1629543341059aae42b5e121d5a4c423ea
Barney <barney.lau@nexa.com.au>
2022-03-31 16:48:30 +1100

    Add Papaparse to parse csv to obj in PasteBox component

--- src/components/PasteBox.js
+++ src/components/PasteBox.js
@@ -1,16 +1,35 @@
-
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import Papa from "papaparse";
 
 function PasteBox() {
-  return (
-    <div>
-			<h2>Paste here</h2>
-			<textarea rows="10" cols="100"></textarea>
-			<br/>
-			<button>Update</button>
-    </div>
-  );
+    const [pasteContents, setTextarea] = useState(
+        "Hello, hello"
+    );
+
+    const handleChange = (event) => {
+        setTextarea(event.target.value);
+    };
+    // let parsedDetails = [];
+    const parseContents = () => {
+				console.log("%c pasteContents:" + pasteContents, "color:pink");
+				let parsedDetails = Papa.parse(pasteContents);
+        console.log("%c parsedDetails", "color:plum", { parsedDetails });
+				
+    };
+    return (
+        <div>
+            <h2>Paste here</h2>
+            <textarea
+                rows="10"
+                cols="100"
+                value={pasteContents}
+                onChange={handleChange}
+            ></textarea>
+            <br />
+            <button onClick={parseContents}>Update</button>
+        </div>
+    );
 }
 
 export default PasteBox;
