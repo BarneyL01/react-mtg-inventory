66401a2781025440cef99685ea50bdcaefa8f866
Barney <barney.lau@nexa.com.au>
2022-03-31 16:05:01 +1100

    Add PasteBox component ui

---
+++ src/components/PasteBox.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+
+function PasteBox() {
+  return (
+    <div>
+			<h2>Paste here</h2>
+			<textarea rows="10" cols="100"></textarea>
+			<br/>
+			<button>Update</button>
+    </div>
+  );
+}
+
+export default PasteBox;
