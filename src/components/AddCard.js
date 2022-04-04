b0a02e3b6bac7630282f69b55032e4a2c1642c8a
Barney <barney.lau@nexa.com.au>
2022-04-04 21:15:17 +1000

    Add fields to add card modal

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -2,13 +2,25 @@ import React from "react";
 // import { useState } from "react";
 import PropTypes from "prop-types";
 
-const AddCard = ({setIsOpen}) => {
+const AddCard = ({ setIsOpen }) => {
     return (
         <>
-            <div className="glass-modal" onClick={() => setIsOpen(false)}>
+            <div className="glass-modal">
                 <div className="modal-content">
-									<header>Hello</header>
-								</div>
+                    <header>Add Card</header>
+                    <button
+                        className="close-button"
+                        onClick={() => setIsOpen(false)}
+                        alt="close"
+                    ></button>
+                    <div>
+                        <input className="search-input"></input>
+                    </div>
+                    <div>
+                        <button className="primary-button">Add</button>
+                        <button className="secondary-button">Cancel</button>
+                    </div>
+                </div>
             </div>
         </>
     );
