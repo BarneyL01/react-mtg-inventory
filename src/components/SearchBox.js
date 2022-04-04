35cf70be852394b66612cbf01421d222dfc925cf
Barney <barney.lau@nexa.com.au>
2022-04-04 23:10:40 +1000

    Fix debounce autocomplete request

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,21 +2,26 @@ import React from "react";
 import { debounce } from "../utils/utils";
 import { scryfallAutocomplete } from "../utils/scryfallApis";
 import AutocompleteSuggestions from "./AutocompleteSuggestions";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 // import PropTypes from "prop-types";
 
 const SearchBox = () => {
     const [userInput, setUserInput] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    const debouncedAutocomplete = useCallback(
+        debounce((newValue) => autocomplete(newValue)),
+        []
+    );
     const updateInput = (newValue) => {
         setUserInput(newValue);
-        debounce(autocomplete(newValue));
+        debouncedAutocomplete(newValue);
     };
     const autocomplete = async function (searchterm) {
         if (searchterm.length < 2) return;
-        // console.log("%c autocomplete:" + searchterm, "color:red");
-				let result = await scryfallAutocomplete(searchterm);
-				setSuggestions(result);
+        console.log("%c autocomplete:" + searchterm, "color:red");
+        let result = await scryfallAutocomplete(searchterm);
+        setSuggestions(result);
+        // setSuggestions(["Hello", "one"]);
     };
     const selectSuggestion = (event) => {
         console.log("%c selectSuggestion:", "color:cyan", {
